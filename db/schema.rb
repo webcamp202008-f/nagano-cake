32aefc3dcd522e4691623bffcf363ab22eaa8575
hiroyuki-ops <hiroyuki.1207.18@gmail.com>
2020-10-01 17:46:38 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_072512) do
+ActiveRecord::Schema.define(version: 2020_10_01_084048) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
