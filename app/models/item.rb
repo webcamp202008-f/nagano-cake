abdc83c01ef6bdf0396a74539bc77986ecf31c94
akihiro1993 <you@example.com>
2020-10-02 15:35:31 +0900

    asociation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  belongs_to :genre
+  has_many :cart_items
+  has_many :order_details
+
+  attachment :image
+  validates :name, {presence: true}
+  validates :description, {presence: true}
+  validates :price, {presence: true}
 end
--- app/models/oder.rb
+++ app/models/oder.rb
@@ -1,2 +1,7 @@
 class Oder < ApplicationRecord
+ belongs_to :customer
+  has_many :order_details
+
+  enum how_to_pay: { "クレジットカード": 0, "銀行振込": 1 }
+  enum order_status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
 end
