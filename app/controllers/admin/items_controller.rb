4a043501b7fef6efc110aac0445ac4f756d4331a
hiroyuki-ops <hiroyuki.1207.18@gmail.com>
2020-10-03 11:42:13 +0900

    not need branch

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,45 @@
+
 class Admin::ItemsController < ApplicationController
+
+
+def index
+  @items = Item.all
+end
+
+def new
+  @items = Item.new
+  end
+
+def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      flash[:genre_created_error] = "ジャンル名を入力してください"
+    redirect_to new_admins_item_path
+    end
+  end
+
+def show
+    @item = Item.find(params[:id])
+  end
+
+def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path(@item)
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      redirect_to edit_admin_item_path(@item)
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_activ)
+  end
 end
