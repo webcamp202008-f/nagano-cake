49bb1c9827ce6c9144c08230a3bcceefa69455df
vagrant <vagrant@localhost.localdomain>
2020-10-03 05:48:13 +0000

    first commit

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,13 @@
 class Public::ItemsController < ApplicationController
+ def index
+  @genres = Genre.all
+  @items = Item.all
+  @items_all = Item.all
+ end
+
+ def show
+  @item = Item.find(params[:id])
+  @cart_item = CartItem.new
+ end
+
 end
