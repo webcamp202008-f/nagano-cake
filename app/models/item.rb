da9412ba48f8095f1f20c15731b6a3fca337c106
akihiro1993 <you@example.com>
2020-10-03 14:31:21 +0900

    items

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
 
   attachment :image
   validates :name, {presence: true}
-  validates :description, {presence: true}
+  validates :introduction, {presence: true}
   validates :price, {presence: true}
 end
