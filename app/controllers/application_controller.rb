0f876a2394bac1fe70900d41baec641f8b147a5e
akihiro1993 <you@example.com>
2020-10-04 11:46:34 +0900

    loginlogout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+
+
+def after_sign_in_path_for(resource)
+  case resource
+  when Admin
+    admin_items_path
+  when Customers
+    new_customer_registration_path
+  end
+end
+
+
+
+
+protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :family_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number])
+    end
 end
