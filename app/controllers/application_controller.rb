b47582088ffc473b29548cac8ba9bd188ec00e3f
akihiro1993 <you@example.com>
2020-10-04 13:55:30 +0900

    second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
 
+before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
 def after_sign_in_path_for(resource)
   case resource
   when Admin
     admin_items_path
-  when Customers
-    new_customer_registration_path
+  when Customer
+    admin_items_path
   end
 end
 
@@ -16,6 +18,6 @@ end
 
 protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :family_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :phone_number])
     end
 end
